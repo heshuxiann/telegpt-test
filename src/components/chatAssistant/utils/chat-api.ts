93e5d91ca536ba9280ff498413c3824a93cde3f6
jason <jsunsu@sending.tech>
2025-07-10 12:02:29 +0800

    fix bug

--- src/components/chatAssistant/utils/chat-api.ts
+++ src/components/chatAssistant/utils/chat-api.ts
@@ -61,6 +61,25 @@ export const summaryMessage = (data: Object) => {
   });
 };
 
+export const globalSummary = (data: Object) => {
+  return new Promise((resolve, reject) => {
+    fetch('https://telegpt-three.vercel.app/global-summary', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
 export const getActionItems = (data: Object) => {
   return new Promise((resolve, reject) => {
     fetch('https://telegpt-three.vercel.app/action-items', {
