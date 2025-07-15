558288e04d482f1e09e24d7e37504c396e614cbb
jason <jsunsu@sending.tech>
2025-07-15 11:20:01 +0800

    onbording

---
+++ src/components/chatAssistant/component/guidance/guidance-carousel.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-null/no-null */
+/* eslint-disable max-len */
+import React, { useRef } from 'react';
+import { Carousel } from 'antd';
+import type { CarouselRef } from 'antd/es/carousel';
+import cx from 'classnames';
+
+import './guidance.scss';
+
+const CarouselItem = ({ className, children }:{ className:string;children?:React.ReactNode }) => {
+  return (
+    <div className={cx('guidance-carousel-item', className)}>
+      <div className="w-[44%] pl-[50px] pt-[5%] pb-[12%] h-full flex flex-col">
+        {children}
+      </div>
+    </div>
+  );
+};
+const GuidanceCarousel = () => {
+  const carouselRef = useRef<CarouselRef>(null);
+  const handleNextClick = (step:number) => {
+    carouselRef.current?.goTo(step);
+  };
+  return (
+    <div>
+      <div className="guidance-bubble" />
+      <Carousel className="guidance-carousel" ref={carouselRef}>
+        <CarouselItem className="guidance-summary">
+          <div className="text-[45px] font-bold leading-[44px] mb-[20px]">Chat Summary</div>
+          <div className="text-[14px]">
+            Message Summarization uses AI to extract key points from IM chats and provide regular summaries, helping users stay updated.
+          </div>
+          <div className="guidance-carousel-next" onClick={() => handleNextClick(1)}>Next</div>
+        </CarouselItem>
+        <CarouselItem className="guidance-translation">
+          <div className="text-[45px] font-bold leading-[44px] mb-[20px]">Translation & Grammar Check</div>
+          <div className="text-[14px]">
+            Instant Translation: Real-time translation with auto language detection.
+            Error Detection: Spots spelling, grammar, and structure issues.
+          </div>
+          <div className="guidance-carousel-next" onClick={() => handleNextClick(2)}>Next</div>
+        </CarouselItem>
+        <CarouselItem className="guidance-meeting">
+          <div className="text-[45px] font-bold leading-[44px] mb-[20px]">Meeting Scheduler</div>
+          <div className="text-[14px]">
+            Automatically detects meeting chats, gathers details, and sends calendar invites—all within the conversation.
+          </div>
+          <div className="guidance-carousel-next" onClick={() => handleNextClick(3)}>Next</div>
+        </CarouselItem>
+        <CarouselItem className="guidance-actions">
+          <div className="text-[45px] font-bold leading-[44px] mb-[20px]">Action Items</div>
+          <div className="text-[14px]">
+            AI picks out tasks and follow-ups from chats in real time—no manual notes needed.
+          </div>
+          <div className="guidance-carousel-next" onClick={() => handleNextClick(4)}>Next</div>
+        </CarouselItem>
+      </Carousel>
+    </div>
+  );
+};
+
+export default GuidanceCarousel;
---
+++ src/components/chatAssistant/component/guidance/guidance-wrapper.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable teactn/no-unused-prop-types */
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable @typescript-eslint/no-use-before-define */
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable no-null/no-null */
+import React, {
+  useEffect,
+  useRef,
+} from '../../../../lib/teact/teact';
+
+import { injectComponent } from '../../../../lib/injectComponent';
+import GuidanceCarousel from './guidance-carousel';
+
+const injectMessageAI = injectComponent(GuidanceCarousel);
+const GuidanceWrapper = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  useEffect(() => {
+    if (containerRef.current) {
+      injectMessageAI(containerRef.current);
+    }
+  }, []);
+  return (
+    <div ref={containerRef} />
+  );
+};
+
+export default GuidanceWrapper;
